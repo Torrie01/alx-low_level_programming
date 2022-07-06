16671875cb71ffabdfcfcb84a7619a87d07e6f78
Torrie01 <t.asifor@yahoo.com>
2022-07-06 07:35:52 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,16 +5,16 @@
 * Return: 0
 **/
 int main(void)
-
 {
 	char ermias[] = "_putchar";
+
 	int c;
 
-		for (c = 0; c < 8; c++)
+	for (c = 0; c < 8; c++)
 
 	{
 
-				_putchar(ermias[c]);
+		_putchar(ermias[c]);
 	}
 
 	_putchar('\n');
